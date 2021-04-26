4f2bd66828c9298c6d8d11493c17a0b5538f5511
Csprier <csprier@gmail.com>
2021-04-26 17:26:36 -0400

    Fixed import syntax of body-parser. Due to some deprecations, methods need to be called a certain way.

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,16 @@
 import express, { Express, Request, Response } from 'express';
-import bodyParser from 'body-parser';
 import helmet from 'helmet';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
 const PORT = process.env.PORT || 8080;
-const app: Express = express();
+const app: Express = express(); // Use Express Type declaration
+const bodyParser = require('body-parser');
 
 app.use(helmet());
-app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.get('/', (req: Request, res: Response) => {
   res.send('<h1>Hello from server.ts!</h1>');
