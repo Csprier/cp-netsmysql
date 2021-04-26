6cde4eab29e391866fb5b5f7cedb10952f083309
Csprier <csprier@gmail.com>
2021-04-26 18:56:08 -0400

    Troubleshooting database connection issues

--- server.js
+++ server.js
@@ -4,16 +4,50 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
-var body_parser_1 = __importDefault(require("body-parser"));
 var helmet_1 = __importDefault(require("helmet"));
 var dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
-var PORT = process.env.PORT || 8080;
-var app = express_1.default();
+var PORT = process.env.PORT;
+var app = express_1.default(); // Use Express Type declaration
+var bodyParser = require('body-parser');
+var mysql = require('mysql');
 app.use(helmet_1.default());
-app.use(body_parser_1.default.json());
-app.use(body_parser_1.default.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+/** POOL */
+var pool = mysql.createPool({
+    host: process.env.HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.database
+});
+pool.getConnection(function (error, connection) {
+    connection.query('SELECT 1 + 1 AS solution', function (error, rows, fields) {
+        if (error)
+            console.error(error);
+        console.log('The solution is: ', rows[0].solution);
+        connection.release();
+    });
+});
+pool.on('connection', function (connection) {
+    console.log('Connected!');
+});
+pool.end(function (error) { return console.error(error); });
 app.get('/', function (req, res) {
     res.send('<h1>Hello from server.ts!</h1>');
 });
 app.listen(PORT, function () { return console.log("Running on http://localhost:" + PORT + "!"); });
+/** CONNECTION */
+// const connection = mysql.createConnection(`mysql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.HOST}:${process.env.DB_PORT}/${process.env.database}`);
+// connection.connect((error: Error) => {
+//   if (error) {
+//     console.error('error connecting: ' + error.stack);
+//     return;
+//   }
+//   console.log('connected as id ' + connection.threadId);
+// });
+// connection.query('SELECT 1 + 1 AS solution', (error: Error, rows: any, fields: any) => {
+//   if (error) console.error(error);
+//   console.log('The solution is: ', rows[0].solution);
+//   connection.release();
+// });
--- src/server.ts
+++ src/server.ts
@@ -4,16 +4,54 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT;
 const app: Express = express(); // Use Express Type declaration
 const bodyParser = require('body-parser');
+const mysql = require('mysql');
 
 app.use(helmet());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+/** POOL */
+const pool = mysql.createPool({
+  host: process.env.HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.database
+});
+
+pool.getConnection((error: Error, connection: any) => {
+  connection.query('SELECT 1 + 1 AS solution', (error: Error, rows: any, fields: any) => {
+    if (error) console.error(error);
+    console.log('The solution is: ', rows[0].solution);
+    connection.release();
+  });
+});
+
+pool.on('connection', (connection: any) => {
+  console.log('Connected!');
+});
+
+pool.end((error: Error) => console.error(error));
+
 app.get('/', (req: Request, res: Response) => {
   res.send('<h1>Hello from server.ts!</h1>');
 });
-
 app.listen(PORT, () => console.log(`Running on http://localhost:${PORT}!`));
+
+/** CONNECTION */
+// const connection = mysql.createConnection(`mysql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.HOST}:${process.env.DB_PORT}/${process.env.database}`);
+// connection.connect((error: Error) => {
+//   if (error) {
+//     console.error('error connecting: ' + error.stack);
+//     return;
+//   }
+//   console.log('connected as id ' + connection.threadId);
+// });
+
+// connection.query('SELECT 1 + 1 AS solution', (error: Error, rows: any, fields: any) => {
+//   if (error) console.error(error);
+//   console.log('The solution is: ', rows[0].solution);
+//   connection.release();
+// });
